64ca680a4a768907fc2bb7866af7ed2042ff2832
swati2904 <swati.lpu17@gmail.com>
2021-07-27 15:47:01 +0530

    initial setup for express

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("GET API working"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
