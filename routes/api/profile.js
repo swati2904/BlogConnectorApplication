a6830a6bf5726a22367089bd016687930f470373
swati2904 <swati.lpu17@gmail.com>
2021-07-28 23:17:19 +0530

    get the currect user proile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-// @route              GET api/profile
-// @description        Test route
-// @acess              Public/Private
+const Profile = require("../../models/Profile");
+// @route              GET api/profile/me
+// @description        GET currectuser profile
+// @acess              Private
 
-router.get("/", (req, res) => res.send("Profile route"));
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
