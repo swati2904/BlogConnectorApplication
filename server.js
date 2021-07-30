09b485ec33cfc8201ef36a67f578a041096a1dd0
swati2904 <swati.lpu17@gmail.com>
2021-07-30 15:59:28 +0530

    port change to 4200

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4200;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
