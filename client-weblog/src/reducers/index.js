cd20499ac22e102ed84e3c6deddca70e7b4b0368
swati2904 <swati.lpu17@gmail.com>
2021-08-09 12:49:01 +0530

    added post action and reducer

--- client-weblog/src/reducers/index.js
+++ client-weblog/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import toast from "./toast";
 import auth from "./auth";
 import profile from "./profile";
+import post from "./post";
 
 export default combineReducers({
   toast,
   auth,
   profile,
+  post,
 });
