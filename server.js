a743bbf3493d167552fb7b77cbb7f5394ddb5a17
swati2904 <swati.lpu17@gmail.com>
2021-08-15 01:52:00 +0530

    ready to deploy the application

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
-
+const path = require("path");
 const app = express();
 
 //Database connection
@@ -9,14 +9,23 @@ connectDB();
 // Init middleware
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("GET API working"));
-
 // Define routes
 
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+//serve static assests in production
+if (process.env.NODE_ENV === "production") {
+  //setstatic folder
+  app.use(express.static("client-weblog/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "client-weblog", "build", "index.html")
+    );
+  });
+}
 
 const PORT = process.env.PORT || 4200;
 
